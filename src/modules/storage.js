fa02ef7027dcaf6185f7722c0f214e19f5a3e208
AndrewNation <andrewpereira.megabyte@gmail.com>
2024-01-20 14:35:31 -0300

    adding logs

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -13,9 +13,12 @@ export function fetchTrash(){
     const localValue = localStorage.getItem("trash")
     if (localValue == null) return []
 
+    console.log(JSON.parse(localValue))
+
     return JSON.parse(localValue)
 }
 
 export function updateTrash(items){
+    console.log('atualizar', trash)
     localStorage.setItem("trash", JSON.stringify(items))
 }
